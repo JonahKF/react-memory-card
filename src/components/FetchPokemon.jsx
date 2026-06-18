f7ccb22ad5931204848bd8e024bdce357a2ebea2
jonah <jonahkf@gmail.com>
2026-06-18 14:42:53 +0900

    list now randomly fetches 25 pokemon

--- src/components/FetchPokemon.jsx
+++ src/components/FetchPokemon.jsx
@@ -2,16 +2,21 @@ import { useState, useEffect } from "react";
 import PokemonCard from "./PokemonCard";
 
 const FetchPokemon = () => {
+  const maxPokemon = 1025;
+
   const [pokemonList, setPokemonList] = useState(null);
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=25")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setPokemonList(data.results);
-      });
+    let pokemonArray = [];
+    for (let i = 0; i < 25; i++) {
+      const randomId = Math.floor(Math.random() * maxPokemon) + 1;
+      pokemonArray.push(
+        fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`).then((res) => {
+          return res.json();
+        }),
+      );
+    }
+    Promise.all(pokemonArray).then((data) => setPokemonList(data));
+    console.log(pokemonList);
   }, []);
 
   if (!pokemonList) return <p>Loading...</p>;
